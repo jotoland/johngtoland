20596f2a7ca619597ff7c8a6519a75457a6df5f5
jtoland <jtoland3.14@gmail.com>
2020-11-04 15:17:43 -0700

    Updates

---
+++ precache-manifest.d2edaa7dfb8e67f347eebe47954f9b61.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "581d7ae3a4f24bab58ca4f7b9cf092d0",
+    "url": "/johngtoland/index.html"
+  },
+  {
+    "revision": "d88b10ea2e7ef5a4bf53",
+    "url": "/johngtoland/static/css/main.7535b0b3.chunk.css"
+  },
+  {
+    "revision": "1d65c0f6c34f3e1ec025",
+    "url": "/johngtoland/static/js/2.81ed581b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/johngtoland/static/js/2.81ed581b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d88b10ea2e7ef5a4bf53",
+    "url": "/johngtoland/static/js/main.8a42581f.chunk.js"
+  },
+  {
+    "revision": "3496e9fd2e4dbc04588c",
+    "url": "/johngtoland/static/js/runtime-main.f7553060.js"
+  },
+  {
+    "revision": "9ce68bcab87f651b5a6bd16489896ef7",
+    "url": "/johngtoland/static/media/GithubLogo.9ce68bca.png"
+  },
+  {
+    "revision": "f08c524d147f46398cb54013ee7013ee",
+    "url": "/johngtoland/static/media/LinkedinLogo.f08c524d.png"
+  },
+  {
+    "revision": "47bf6876899312a2992dece37be18a31",
+    "url": "/johngtoland/static/media/locus.47bf6876.png"
+  },
+  {
+    "revision": "3512ada4000147e3227dcd3b4476bc24",
+    "url": "/johngtoland/static/media/mariobros.3512ada4.png"
+  },
+  {
+    "revision": "571177cec75b6d1a4c89f0e9feead787",
+    "url": "/johngtoland/static/media/minesweep.571177ce.png"
+  },
+  {
+    "revision": "cd81f26d141f670a1397181b8ff3124b",
+    "url": "/johngtoland/static/media/name-logo.cd81f26d.png"
+  },
+  {
+    "revision": "51f39e12413e9ca425272683faaad1a0",
+    "url": "/johngtoland/static/media/networksecurity.51f39e12.jpg"
+  },
+  {
+    "revision": "3fd607a157456d64c864fa94805e99a7",
+    "url": "/johngtoland/static/media/profilepic.3fd607a1.jpg"
+  },
+  {
+    "revision": "fe3d5d7ad90159d4c3f5dd0f2b51c98a",
+    "url": "/johngtoland/static/media/rush.fe3d5d7a.png"
+  },
+  {
+    "revision": "1eb07f3e53f2d225c8fcd7c61e376387",
+    "url": "/johngtoland/static/media/uot.1eb07f3e.png"
+  }
+]);
